2fccb1cc3e97253281fa9d9c0acaed24921ddf6a
IT21029868 <it21029868@my.sliit.lk>
2022-10-26 18:47:02 +0530

    new pages added

--- app/src/main/java/com/example/sparkx/AddToCart.java
+++ app/src/main/java/com/example/sparkx/AddToCart.java
@@ -3,19 +3,43 @@ package com.example.sparkx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddToCart extends AppCompatActivity {
-
+    private Button button34;
     @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_cart);
 
+
+
+
+
+
         getSupportActionBar().setTitle("Add To Cart");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.actionbar)));
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+
+        button34 = findViewById(R.id.button34);
+        button34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFindAStay();
+            }
+        });
     }
+    public void openFindAStay(){
+        Intent intent = new Intent(this, FindAStay.class);
+        startActivity(intent);
+    }
+
 }
